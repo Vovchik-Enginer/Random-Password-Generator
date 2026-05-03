86adf4287a12ae7dbff100d7eeafa524b9d402ee
Proger <lvvasilenko1977@mail.ru>
2026-05-03 20:59:18 +0400

    Импорт библиотек и создание главного окна

--- main.py
+++ main.py
@@ -2,6 +2,23 @@
 
 # GUI-приложение «Random Password Generator» ("Генератор случайных паролей")
 
+# Импорт необходимых библиотек
+import tkinter as tk
+from tkinter import ttk
+import random
+import string
+import json
+from datetime import datetime
 
 
+# Главное окно
+root = tk.Tk()
+root.title("Генератор случайных паролей")
+root.geometry("700x900")
+root.configure(bg="#b8e9b6")
+
+
+
+
+root.mainloop()
 
