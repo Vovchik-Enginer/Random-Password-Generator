33185ce5799f317befb339c2aaea353ccc811100
Proger <lvvasilenko1977@mail.ru>
2026-05-03 21:15:53 +0400

    Add variable and instuction for user

--- main.py
+++ main.py
@@ -18,8 +18,32 @@ root.geometry("700x900")
 root.configure(bg="#b8e9b6")
 
 
+# Переменная длины пароля
+length_pasw = tk.IntVar(value=4)  # начальное значение 4 (для сверхпростого пароля)
+
+# Переменные для чекбоксов
+lowercase_var = tk.BooleanVar(value=True) # по умолчанию один из блоков включен изначально, чтобы избежать ошибки генерации
+uppercase_var = tk.BooleanVar(value=False)
+digits_var = tk.BooleanVar(value=False)
+special_var = tk.BooleanVar(value=False)
 
 
+
+
+
+# Метка приветствия
+label_intro = tk.Label(root, text="Добро пожаловать в программу генерации надежных паролей!",  font=("Arial", 16, "bold"), bg="#b8e9b6", wraplength=450)
+label_intro.pack(pady=10)
+
+# Метка-инструкция для пользователя
+label_1 = tk.Label(root, text="Выберите нужные компоненты пароля:", font=("Arial", 12, "bold"), bg="#b8e9b6")
+label_1.pack(pady=10)
+
+# Чекбоксы
+tk.Checkbutton(root, text="строчные буквы (a-z)", variable=lowercase_var).pack(padx=50, pady=3, anchor="w")
+tk.Checkbutton(root, text="ЗАГЛАВНЫЕ БУКВЫ (A-Z)", variable=uppercase_var).pack(padx=50, pady=3, anchor="w")
+tk.Checkbutton(root, text="Цифры (0-9)", variable=digits_var).pack(padx=50, pady=3, anchor="w")
+tk.Checkbutton(root, text="Спецсимволы", variable=special_var).pack(padx=50, pady=3, anchor="w")
 
 
 
